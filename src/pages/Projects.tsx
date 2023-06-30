56d451014df756e6c7acfb384c8df2a40828166a
dhwani22 <dhwanisheth2202@gmail.com>
2023-06-30 17:41:16 +0530

    df

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,7 +5,7 @@ function Projects() {
   return (
     <div className="overflow-x-hidden">
       <TopBar />
-      <div className="xl:h-[8.651474530831099vh] lg:mt-[9.651474530831099vh] xs:mt-[9.051474530831099vh]">
+      <div className="xl:mt-[8.651474530831099vh] lg:mt-[9.651474530831099vh] xs:mt-[9.051474530831099vh]">
         <ProjectsDetailMainPage />
       </div>
     </div>
