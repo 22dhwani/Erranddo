f3e3b6ab125eb09a4a677dc456f107121e4c8b00
Durva <durvabrahmbhatt2016@gmail.com>
2023-07-03 15:40:14 +0530

    changes

--- src/components/home/HomeTopBar.tsx
+++ src/components/home/HomeTopBar.tsx
@@ -1,52 +1,56 @@
 import ErranddoLogo from "../../assets/Group 1@3x.png";
 import { Navigate, useNavigate } from "react-router";
+import Button from "../UI/Button";
 
 function HomeTopBar() {
   const navigate = useNavigate();
+  const topbarClassName =
+    "bg-white dark:bg-black fixed top-0 py-4 xl:px-36 lg:px-24 xs:px-5 flex shadow-md justify-between w-screen items-center lg:h-[9.651474530831099vh] xl:h-[8.651474530831099vh] xs:h-[9.051474530831099vh] z-[100]";
   return (
-    <nav className="bg-white dark:bg-black fixed w-screen z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen flex flex-wrap items-center justify-between p-4">
-        <div className="2xl:pl-56 xl:pl-36 lg:pl-32">
+    <div className={topbarClassName}>
+      <div className="my-1 xs:w-3/6 lg:w-max">
+        <button onClick={() => navigate("/home")}>
           <img
             src={ErranddoLogo}
-            className="xl:h-8 lg:h-6 xs:h-5"
-            alt="Erranddo Logo"
+            className="lg:w-80 xs:w-full object-contain"
           />
+        </button>
+      </div>
+      <div className="md:flex items-center md:gap-5 xs:hidden">
+        <div className="pr-5">
+          <ul className="flex gap-3 flex-col mt-4 xl:text-lg md:text-sm border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-black dark:text-semibold">
+            <li>
+              <button
+                className="text-[#707070] dark:text-white"
+                onClick={() => navigate("/signup-customer")}
+              >
+                Sign Up
+              </button>
+            </li>
+            <div className="xl:h-6 md:h-5 min-h-[1em] w-0.5 bg-[#707070] opacity-40"></div>
+            <li>
+              <button
+                className="text-[#707070] dark:text-white"
+                onClick={() => navigate("/sign-in")}
+              >
+                Sign In
+              </button>
+            </li>
+          </ul>
         </div>
-        <div className="md:flex items-center xl:pr-56 md:pr-36 xs:hidden">
-          <div className="pr-5">
-            <ul className="flex gap-3 flex-col mt-4 xl:text-lg md:text-sm border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-black dark:text-semibold">
-              <li>
-                <button
-                  className="text-[#707070]  dark:text-white"
-                  onClick={() => navigate("/signup-customer")}
-                >
-                  Sign Up
-                </button>
-              </li>
-              <div className=" xl:h-6 md:h-5 min-h-[1em] w-0.5 bg-[#707070] opacity-40"></div>
-              <li>
-                <button
-                  className="text-[#707070]  dark:text-white "
-                  onClick={() => navigate("/sign-in")}
-                >
-                  Sign In
-                </button>
-              </li>
-            </ul>
-          </div>
-          <div className="">
-            <button
-              type="button"
-              className="text-white bg-[#0003FF] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 xl:text-lg md:text-sm rounded-lg px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              onClick={() => navigate("/signup-pro")}
-            >
-              Register as a Pro
-            </button>
-          </div>
+        <div className="hidden xs:block md:block">
+          <Button
+            variant="filled"
+            color="primary"
+            type="button"
+            buttonClassName="xs:text-xs lg:text-base rounded-lg py-2 text-center md:mr-0"
+            onClick={() => navigate("/signup-pro")}
+          >
+            Register as a Pro
+          </Button>
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
 
