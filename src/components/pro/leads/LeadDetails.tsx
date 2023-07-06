5739f4b7e2e359d8b72f2d8c5ec3319d77fade0e
dhwani22 <dhwanisheth2202@gmail.com>
2023-07-06 17:58:52 +0530

    changes

--- src/components/pro/leads/LeadDetails.tsx
+++ src/components/pro/leads/LeadDetails.tsx
@@ -1,111 +1,108 @@
 import Heading from "../../UI/Heading";
 import HomeCard from "../dashboard/home/HomeCard";
-import ProfileImage from "../../../assets/service-image-one.png";
-import GreenTick from "../../../assets/GreenTick.svg";
-import GreenRoundTick from "../../../assets/GreenRoundTick.svg";
-import BlackRoundTick from "../../../assets/BlackRoundTick.svg";
+
+import PhotoOne from "../../../assets/photo-one.png";
+import PhotoTwo from "../../../assets/photo-two.png";
+import PhotoThree from "../../../assets/photo-three.png";
 
 function LeadDetails() {
   return (
     <HomeCard className="rounded-md  px-5 pb-10 mt-5">
       <div className="py-4 border-b-[0.5px] border-b-slate-200">
         <Heading
-          text={`My Leads`}
+          text={`Leads Details`}
           variant="subHeader"
           headingclassName="!font-bold  text-textColor  text-xl tracking-wide "
         />
       </div>
-      <div className="flex justify-between py-4 border-b-[0.5px] border-b-slate-200 ">
-        <div className="flex items-center gap-2">
-          <img src={ProfileImage} className="" />
-          <div className="flex flex-col">
-            <Heading
-              text={"TV Guru Limited"}
-              variant="subTitle"
-              headingclassName="!font-semibold  !text-lg mx-1 tracking-wide dark:text-white "
-            />
-            <Heading
-              text={`TV Wall Mounting`}
-              variant="subHeader"
-              headingclassName="!font-normal !text-sm mx-1 text-textColor tracking-wide dark:text-white"
-            />
-          </div>
-        </div>
-        <Heading
-          text={`Posted 10min ago`}
-          variant="subHeader"
-          headingclassName="!font-medium !text-sm mt-2 text-primaryBlue tracking-wide dark:text-white"
-        />
-      </div>
+
       <div className="py-4 grid lg:grid-cols-2 ">
         <div>
           <Heading
-            text={"Name"}
+            text={"How big is your TV?"}
             variant="subTitle"
             headingclassName="!font-semibold text-slate-400 !text-sm  mx-1 tracking-wide dark:text-white "
           />
           <Heading
-            text={`Pter James`}
+            text={`55 Inch TV`}
             variant="subHeader"
             headingclassName="!font-normal !text-lg mx-1 text-textColor tracking-wide dark:text-white"
           />
         </div>
         <div>
           <Heading
-            text={"Location"}
+            text={"Wall type ?"}
             variant="subTitle"
             headingclassName="!font-semibold text-slate-400 !text-sm  mx-1 tracking-wide dark:text-white "
           />
           <Heading
-            text={`London, SE18`}
+            text={`Brick Wall`}
             variant="subHeader"
             headingclassName="!font-normal !text-lg mx-1 text-textColor tracking-wide dark:text-white"
           />
         </div>
       </div>
-      <div className="lg:py-4 grid lg:grid-cols-2 border-b-[0.5px] border-b-slate-200 ">
+      <div className="lg:py-4 grid lg:grid-cols-2 ">
         <div>
           <Heading
-            text={"Tel"}
+            text={"Have you got a bracket?"}
             variant="subTitle"
             headingclassName="!font-semibold text-slate-400 !text-sm  mx-1 tracking-wide dark:text-white "
           />
-          <div className="flex gap-3">
-            <Heading
-              text={`094*****23*`}
-              variant="subHeader"
-              headingclassName="!font-normal !text-lg mx-1 text-textColor tracking-wide dark:text-white"
-            />
-            <img src={GreenTick} />
-          </div>
+
+          <Heading
+            text={`Cable Concealing options?`}
+            variant="subHeader"
+            headingclassName="!font-normal !text-lg mx-1 text-textColor tracking-wide dark:text-white"
+          />
         </div>
         <div>
           <Heading
-            text={"Email"}
+            text={"Cable Concealing options?"}
             variant="subTitle"
             headingclassName="!font-semibold text-slate-400 !text-sm  mx-1 tracking-wide dark:text-white "
           />
-          <div className="flex gap-3">
-            <Heading
-              text={`dhwan*****@.com`}
-              variant="subHeader"
-              headingclassName="!font-normal !text-lg mx-1 text-textColor tracking-wide dark:text-white"
-            />
-            <img src={GreenTick} />
-          </div>
+
+          <Heading
+            text={`Cable concealing in surface plastic...`}
+            variant="subHeader"
+            headingclassName="!font-normal !text-lg mx-1 text-textColor tracking-wide dark:text-white"
+          />
         </div>
       </div>
-      <div className="pt-4">
-        <Heading
-          text={`Only 4 Pro’s can reply to this lead`}
-          variant="subHeader"
-          headingclassName="!font-normal !text-lg mx-1 text-textColor tracking-wide dark:text-white"
-        />
-        <div className="flex gap-2 my-1">
-          <img src={GreenRoundTick} />
-          <img src={GreenRoundTick} />
-          <img src={BlackRoundTick} />
-          <img src={BlackRoundTick} />
+      <div className="lg:py-4 grid lg:grid-cols-2 ">
+        <div>
+          <Heading
+            text={"Comments / Photos"}
+            variant="subTitle"
+            headingclassName="!font-semibold text-slate-400 !text-sm  mx-1 tracking-wide dark:text-white "
+          />
+
+          <Heading
+            text={`I would like a call for further details`}
+            variant="subHeader"
+            headingclassName="!font-normal !text-lg mx-1 text-textColor tracking-wide dark:text-white"
+          />
+        </div>
+      </div>
+      <div className="py-4 flex justify-between gap-5">
+        <div>
+          <img
+            src={PhotoOne}
+            className="lg:h-60 md:h-36 xs:h-28 w-full object-cover"
+          />
+        </div>
+        <div>
+          <img
+            src={PhotoTwo}
+            className="lg:h-60 md:h-36 xs:h-28 w-full object-cover"
+          />
+        </div>
+        <div>
+          <img
+            src={PhotoThree}
+            className="lg:h-60 md:h-36 xs:h-28 w-full object-cover"
+          />
         </div>
       </div>
     </HomeCard>
