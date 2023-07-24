f34dbb3f381bba393cf141b41ec2a6ebf11d4dd9
Durva <durvabrahmbhatt2016@gmail.com>
2023-07-24 15:04:30 +0530

    changes

--- src/components/pro/settings/SettingsNavLinkPro.tsx
+++ src/components/pro/settings/SettingsNavLinkPro.tsx
@@ -4,126 +4,142 @@ import Heading from "../../UI/Heading";
 function SettingsNavLinkPro() {
   return (
     <div className="w-full flex justify-between">
-      <div className="  grid grid-cols-6 md:py-3 xs:py-5 xs:w-full xs:gap-3 lg:gap-0 px-5">
-        <div className="w-full">
-          <NavLink
-            className="hover:text-primaryBlue  flex w-fit "
-            style={({ isActive }) =>
-              isActive
-                ? {
-                    color: "#0003FF",
-                    borderBottom: "3px #0003FF  solid",
+      <div className="md:py-3 xs:py-2 xs:w-full px-5">
+        <div className="navbar w-full overflow-x-auto scrollbar-thumb-gray-300 scrollbar-track-gray-100">
+          <ul className="flex lg:justify-between xs:gap-6 list-none p-0 m-0">
+            <li>
+              <div className="xs:w-28 md:w-full">
+                <NavLink
+                  className="hover:text-primaryBlue  flex w-fit "
+                  style={({ isActive }) =>
+                    isActive
+                      ? {
+                          color: "#0003FF",
+                          borderBottom: "3px #0003FF  solid",
+                        }
+                      : {}
                   }
-                : {}
-            }
-            to="/pro/settings/personal-info"
-          >
-            <Heading
-              text={"Personal Info"}
-              variant="subHeader"
-              headingclassName=" !font-semibold tracking-wide md:text-base  xs:text-xs sm:text-sm dark:text-white"
-            />
-          </NavLink>
-        </div>
-        <div className="w-full">
-          <NavLink
-            className="hover:text-primaryBlue  flex  w-fit mx-auto"
-            style={({ isActive }) =>
-              isActive
-                ? {
-                    color: "#0003FF",
-                    borderBottom: "3px #0003FF  solid",
+                  to="/pro/settings/personal-info"
+                >
+                  <Heading
+                    text={"Personal Info"}
+                    variant="subHeader"
+                    headingclassName=" !font-semibold tracking-wide md:text-base  xs:text-base sm:text-base dark:text-white"
+                  />
+                </NavLink>
+              </div>
+            </li>
+            <li className="">
+              <div className="xs:w-36 md:w-full">
+                <NavLink
+                  className="hover:text-primaryBlue  flex  w-fit mx-auto"
+                  style={({ isActive }) =>
+                    isActive
+                      ? {
+                          color: "#0003FF",
+                          borderBottom: "3px #0003FF  solid",
+                        }
+                      : {}
                   }
-                : {}
-            }
-            to="/pro/settings/contact-details"
-          >
-            <Heading
-              text={"Contact Details"}
-              variant="subHeader"
-              headingclassName="dark:hover:text-primaryBlue !font-semibold tracking-wide md:text-base  xs:text-xs sm:text-sm dark:text-white"
-            />
-          </NavLink>
-        </div>
-        <div className="w-full  ">
-          <NavLink
-            className="hover:text-primaryBlue  flex  w-fit mx-auto"
-            style={({ isActive }) =>
-              isActive
-                ? {
-                    color: "#0003FF",
-                    borderBottom: "3px #0003FF  solid",
+                  to="/pro/settings/contact-details"
+                >
+                  <Heading
+                    text={"Contact Details"}
+                    variant="subHeader"
+                    headingclassName="dark:hover:text-primaryBlue !font-semibold tracking-wide md:text-base  xs:text-base sm:text-base dark:text-white"
+                  />
+                </NavLink>
+              </div>
+            </li>
+            <li className="">
+              <div className="xs:w-36 md:w-full">
+                <NavLink
+                  className="hover:text-primaryBlue  flex  w-fit ml-auto"
+                  style={({ isActive }) =>
+                    isActive
+                      ? {
+                          color: "#0003FF",
+                          borderBottom: "3px #0003FF  solid",
+                        }
+                      : {}
                   }
-                : {}
-            }
-            to="/pro/settings/password"
-          >
-            <Heading
-              text={"Password"}
-              variant="subHeader"
-              headingclassName="dark:hover:text-primaryBlue !font-semibold tracking-wide md:text-base  xs:text-xs sm:text-sm dark:text-white"
-            />
-          </NavLink>
-        </div>
-        <div className="w-full  ">
-          <NavLink
-            className="hover:text-primaryBlue  flex  w-fit ml-auto"
-            style={({ isActive }) =>
-              isActive
-                ? {
-                    color: "#0003FF",
-                    borderBottom: "3px #0003FF  solid",
+                  to="/pro/settings/payment-details"
+                >
+                  <Heading
+                    text={"Payment Details"}
+                    variant="subHeader"
+                    headingclassName="dark:hover:text-primaryBlue !font-semibold tracking-wide md:text-base  xs:text-base  dark:text-white"
+                  />
+                </NavLink>
+              </div>
+            </li>
+            <li className="">
+              <div className="xs:w-24 md:w-full">
+                <NavLink
+                  className="hover:text-primaryBlue  flex  w-fit mx-auto"
+                  style={({ isActive }) =>
+                    isActive
+                      ? {
+                          color: "#0003FF",
+                          borderBottom: "3px #0003FF  solid",
+                        }
+                      : {}
                   }
-                : {}
-            }
-            to="/pro/settings/payment-details"
-          >
-            <Heading
-              text={"Payment Details"}
-              variant="subHeader"
-              headingclassName="dark:hover:text-primaryBlue !font-semibold tracking-wide md:text-base  xs:text-xs sm:text-sm dark:text-white"
-            />
-          </NavLink>
-        </div>
-        <div className="w-full">
-          <NavLink
-            className="hover:text-primaryBlue  flex  w-fit ml-auto"
-            style={({ isActive }) =>
-              isActive
-                ? {
-                    color: "#0003FF",
-                    borderBottom: "3px #0003FF  solid",
+                  to="/pro/settings/password"
+                >
+                  <Heading
+                    text={"Password"}
+                    variant="subHeader"
+                    headingclassName="dark:hover:text-primaryBlue !font-semibold tracking-wide md:text-base  xs:text-base sm:text-base dark:text-white"
+                  />
+                </NavLink>
+              </div>
+            </li>
+            <li className="">
+              <div className="w-full">
+                <NavLink
+                  className="hover:text-primaryBlue  flex  w-fit ml-auto"
+                  style={({ isActive }) =>
+                    isActive
+                      ? {
+                          color: "#0003FF",
+                          borderBottom: "3px #0003FF  solid",
+                        }
+                      : {}
                   }
-                : {}
-            }
-            to="/pro/settings/credits"
-          >
-            <Heading
-              text={"Credits"}
-              variant="subHeader"
-              headingclassName="dark:hover:text-primaryBlue !font-semibold tracking-wide md:text-base  xs:text-xs sm:text-sm dark:text-white"
-            />
-          </NavLink>
-        </div>
-        <div className="w-full">
-          <NavLink
-            className="hover:text-primaryBlue flex w-fit  ml-auto"
-            style={({ isActive }) =>
-              isActive
-                ? {
-                    color: "#0003FF",
-                    borderBottom: "3px #0003FF  solid",
+                  to="/pro/settings/credits"
+                >
+                  <Heading
+                    text={"Credits"}
+                    variant="subHeader"
+                    headingclassName="dark:hover:text-primaryBlue !font-semibold tracking-wide md:text-base  xs:text-base sm:text-base dark:text-white"
+                  />
+                </NavLink>
+              </div>
+            </li>
+            <li className="">
+              <div className="w-full">
+                <NavLink
+                  className="hover:text-primaryBlue flex w-fit ml-auto"
+                  style={({ isActive }) =>
+                    isActive
+                      ? {
+                          color: "#0003FF",
+                          borderBottom: "3px #0003FF  solid",
+                        }
+                      : {}
                   }
-                : {}
-            }
-            to="/pro/settings/invoices"
-          >
-            <Heading
-              text={"Invoices"}
-              variant="subHeader"
-              headingclassName="dark:hover:text-primaryBlue !font-semibold tracking-wide md:text-base  xs:text-xs sm:text-sm dark:text-white"
-            />
-          </NavLink>
+                  to="/pro/settings/invoices"
+                >
+                  <Heading
+                    text={"Invoices"}
+                    variant="subHeader"
+                    headingclassName="dark:hover:text-primaryBlue !font-semibold tracking-wide md:text-base  xs:text-base sm:text-base dark:text-white"
+                  />
+                </NavLink>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
