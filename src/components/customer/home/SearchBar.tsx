a2885b4c39bace6c54f6b277707efce115bf1ea7
Dhrumil Shah <dhrumilshah672001@gmail.com>
2023-08-02 15:12:18 +0530

    Fix: border issue

--- src/components/customer/home/SearchBar.tsx
+++ src/components/customer/home/SearchBar.tsx
@@ -14,7 +14,7 @@ const SearchBar = (props: { onChange: (key: string) => void; key: string }) => {
   const { theme } = useTheme();
 
   const searchBarClass =
-    "flex items-center md:w-96 lg:w-80 xl:w-96 xs:w-full text-md  font-semibold font-poppins text-textColor dark:bg-black bg-white px-2 my-3 py-2  hover:border-[0.3px] hover:border-gray-400 rounded-xl drop-shadow-sm    ease-in focus:caret-slate-500  lg:mr-3  ";
+    "flex items-center md:w-96 lg:w-80 xl:w-96 xs:w-full text-md  font-semibold font-poppins text-textColor dark:bg-black bg-white px-2 my-3 py-2  hover:ring-1 hover:ring-gray-400 rounded-xl drop-shadow-sm    ease-in focus:caret-slate-500  lg:mr-3  ";
   return (
     <form className="">
       <div className="flex items-center gap-2">
@@ -28,8 +28,8 @@ const SearchBar = (props: { onChange: (key: string) => void; key: string }) => {
           </button>
           <input
             value={`${localStorage.getItem("service")
-                ? JSON.parse(localStorage.getItem("service") ?? "").name
-                : searchKey
+              ? JSON.parse(localStorage.getItem("service") ?? "").name
+              : searchKey
               }`}
             placeholder={`${localStorage.getItem("service") ?? "Search"}`}
             className="focus:outline-none w-full placeholder:text-md placeholder:font-normal  bg-white dark:text-white dark:bg-black dark:placeholder:text-white"
