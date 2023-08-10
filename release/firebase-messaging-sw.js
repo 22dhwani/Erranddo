5b32ff2a165a7eac21951eb90d86c914eb97b8e6
Dhrumil Shah <dhrumilshah672001@gmail.com>
2023-08-10 10:36:03 +0530

    new build

--- release/firebase-messaging-sw.js
+++ release/firebase-messaging-sw.js
@@ -1,17 +1,17 @@
 // eslint-disable-next-line no-undef
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
 // eslint-disable-next-line no-undef
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-    apiKey: "AIzaSyAiFWJ2KbaBth5pX3Rv084a6mhUHPUsP34",
-    authDomain: "erranddo-36cbc.firebaseapp.com",
-    projectId: "erranddo-36cbc",
-    storageBucket: "erranddo-36cbc.appspot.com",
-    messagingSenderId: "715070118259",
-    appId: "1:715070118259:web:52fc23bee61f6fce940d06",
-    measurementId: "G-8X4TWTJKE3"
+  apiKey: "AIzaSyAiFWJ2KbaBth5pX3Rv084a6mhUHPUsP34",
+  authDomain: "erranddo-36cbc.firebaseapp.com",
+  projectId: "erranddo-36cbc",
+  storageBucket: "erranddo-36cbc.appspot.com",
+  messagingSenderId: "715070118259",
+  appId: "1:715070118259:web:52fc23bee61f6fce940d06",
+  measurementId: "G-8X4TWTJKE3",
 };
 
 // eslint-disable-next-line no-undef
@@ -22,13 +22,11 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function (payload) {
-    console.log('Received background message ', payload);
-    // Customize notification here
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-    };
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
 
-    self.registration.showNotification(notificationTitle,
-        notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
