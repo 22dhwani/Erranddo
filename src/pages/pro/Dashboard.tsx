0d9c06df68bb8d26e15fade1385418100c7a0a1f
Durva <durvabrahmbhatt2016@gmail.com>
2023-09-13 12:19:42 +0530

    bugs:fixed

--- src/pages/pro/Dashboard.tsx
+++ src/pages/pro/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import SideBar from "../../components/pro/dashboard/side-bar/SideBar";
 import TopBar from "../../components/pro/dashboard/top-bar/TopBar";
 import MobileSideBar from "../../components/pro/dashboard/mobile-sidebar/MobileSideBar";
@@ -6,14 +6,23 @@ import BusinessContextProvider from "../../store/pro/dashboard-context";
 import ServiceContextProvider from "../../store/pro/service-context";
 import ReviewContextProProvider from "../../store/pro/review-context";
 
+// const currentPath = window.location.pathname;
+// const isPersonalInfoRoute = currentPath === "/pro/settings/personal-info";
+
+function useIsPersonalInfoRoute() {
+  const location = useLocation();
+  return location.pathname === "/pro/settings/personal-info";
+}
+
 function Dashboard() {
+  const isPersonalInfoRoute = useIsPersonalInfoRoute();
   return (
     <ServiceContextProvider>
       <BusinessContextProvider>
         <ReviewContextProProvider>
           <div className="overflow-x-hidden">
             <div>
-              <TopBar />
+              <TopBar isSettingDisabled={isPersonalInfoRoute ?? false} />
               <div className="h-screen flex flex-row w-screen">
                 <SideBar />
                 <Outlet />
