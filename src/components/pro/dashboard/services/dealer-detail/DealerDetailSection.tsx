21de10c662ab4c24cd0b5e7aee428757619ae55b
Durva <durvabrahmbhatt2016@gmail.com>
2023-09-13 16:28:40 +0530

    chan

--- src/components/pro/dashboard/services/dealer-detail/DealerDetailSection.tsx
+++ src/components/pro/dashboard/services/dealer-detail/DealerDetailSection.tsx
@@ -38,8 +38,7 @@ function DealerDetailSection(props: {
   const [show, setShow] = useState(false);
   const disableEmailsAndLinks = (text: any) => {
     const emailRegex = /\S+@\S+\.\S+/g;
-    const urlRegex =
-      /(?:https?|ftp):\/\/[\n\S]+|www\.[\S]+\.[a-z]+|[a-z]+.com+/g;
+    const urlRegex = /(?:https?|ftp):\/\/[\n\S]+|www\.[\S]+\.[a-z]+/g;
     const phoneRegex = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/g;
     const blurredText = text.replace(
       emailRegex,
