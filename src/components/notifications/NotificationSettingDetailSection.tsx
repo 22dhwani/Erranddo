788fac9f68a3faa8cb03f3157085f4b8e106bf81
Durva <durvabrahmbhatt2016@gmail.com>
2023-09-18 10:00:11 +0530

    chnages

--- src/components/notifications/NotificationSettingDetailSection.tsx
+++ src/components/notifications/NotificationSettingDetailSection.tsx
@@ -4,58 +4,61 @@ import NotificationSettingHeading from "./NotificationSettingHeading";
 
 function NotificationSettingDetailSection() {
   const { userData, isDetailLoading } = useAuth();
+  console.log(userData, "this is");
 
   return (
     <div>
       <NotificationSettingHeading />
       {!isDetailLoading && (
-        <NotificationSetting
-          appStatus={
-            userData?.metadata?.is_app_request_creation_notification_on == 1
-              ? true
-              : false
-          }
-          emailStatus={
-            userData?.metadata?.is_app_request_creation_notification_on == 1
-              ? true
-              : false
-          }
-          question={"When a request has been created or close"}
-          appKey="is_app_request_creation_notification_on"
-          emailKey="is_email_request_creation_notification_on"
-        />
-      )}
-      <NotificationSetting
-        appStatus={
-          userData?.metadata?.is_app_recieved_quote_notification_on == 1
-            ? true
-            : false
-        }
-        emailStatus={
-          userData?.metadata?.is_email_recieved_quote_notification_on == 1
-            ? true
-            : false
-        }
-        question={"When I receive a message or quote"}
-        appKey="is_app_recieved_quote_notification_on"
-        emailKey="is_email_recieved_quote_notification_on"
-      />
+        <div>
+          <NotificationSetting
+            appStatus={
+              userData?.metadata?.is_app_request_creation_notification_on == 1
+                ? true
+                : false
+            }
+            emailStatus={
+              userData?.metadata?.is_app_request_creation_notification_on == 1
+                ? true
+                : false
+            }
+            question={"When a request has been created or close"}
+            appKey="is_app_request_creation_notification_on"
+            emailKey="is_email_request_creation_notification_on"
+          />
+          <NotificationSetting
+            appStatus={
+              userData?.metadata?.is_app_recieved_quote_notification_on == 1
+                ? true
+                : false
+            }
+            emailStatus={
+              userData?.metadata?.is_email_recieved_quote_notification_on == 1
+                ? true
+                : false
+            }
+            question={"When I receive a message or quote"}
+            appKey="is_app_recieved_quote_notification_on"
+            emailKey="is_email_recieved_quote_notification_on"
+          />
 
-      <NotificationSetting
-        appStatus={
-          userData?.metadata?.is_app_promotion_mail_notification_on == 1
-            ? true
-            : false
-        }
-        emailStatus={
-          userData?.metadata?.is_email_promotion_mail_notification_on == 1
-            ? true
-            : false
-        }
-        question={"Occasional promotional emails"}
-        appKey="is_app_promotion_mail_notification_on"
-        emailKey="is_email_promotion_mail_notification_on"
-      />
+          <NotificationSetting
+            appStatus={
+              userData?.metadata?.is_app_promotion_mail_notification_on == 1
+                ? true
+                : false
+            }
+            emailStatus={
+              userData?.metadata?.is_email_promotion_mail_notification_on == 1
+                ? true
+                : false
+            }
+            question={"Occasional promotional emails"}
+            appKey="is_app_promotion_mail_notification_on"
+            emailKey="is_email_promotion_mail_notification_on"
+          />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/pro/notifications/NotificationSettingDetailSectionPro.tsx
+++ src/components/pro/notifications/NotificationSettingDetailSectionPro.tsx
@@ -3,51 +3,59 @@ import NotificationSettingHeadingPro from "./NotificationSettingHeadingPro";
 import { useAuthPro } from "../../../store/pro/auth-pro-context";
 
 function NotificationSettingDetailSectionPro() {
-  const { userData } = useAuthPro();
+  const { userData, isDetailLoading } = useAuthPro();
 
   return (
     <div>
       <NotificationSettingHeadingPro />
-      <NotificationSettingPro
-        appStatus={
-          userData?.metadata?.is_app_lead_notification_on == 1 ? true : false
-        }
-        emailStatus={
-          userData?.metadata?.is_email_lead_notification_on == 1 ? true : false
-        }
-        question={"When a new lead is posted that matches my niche "}
-        appKey="is_app_lead_notification_on"
-        emailKey="is_email_lead_notification_on"
-      />
-      <NotificationSettingPro
-        appStatus={
-          userData?.metadata?.is_app_request_quote_notification_on == 1
-            ? true
-            : false
-        }
-        emailStatus={
-          userData?.metadata?.is_email_request_quote_notification_on == 1
-            ? true
-            : false
-        }
-        question={"When a customer requests a quote"}
-        appKey="is_app_request_quote_notification_on"
-        emailKey="is_email_request_quote_notification_on"
-      />
-      <NotificationSettingPro
-        appStatus={
-          userData?.metadata?.is_app_review_notification_on == 1 ? true : false
-        }
-        emailStatus={
-          userData?.metadata?.is_email_review_notification_on == 1
-            ? true
-            : false
-        }
-        question={"When a review has been received "}
-        appKey="is_app_review_notification_on"
-        emailKey="is_email_review_notification_on"
-      />
-      {/* <NotificationSettingPro
+      {!isDetailLoading && (
+        <div>
+          <NotificationSettingPro
+            appStatus={
+              userData?.metadata?.is_app_lead_notification_on == 1
+                ? true
+                : false
+            }
+            emailStatus={
+              userData?.metadata?.is_email_lead_notification_on == 1
+                ? true
+                : false
+            }
+            question={"When a new lead is posted that matches my niche "}
+            appKey="is_app_lead_notification_on"
+            emailKey="is_email_lead_notification_on"
+          />
+          <NotificationSettingPro
+            appStatus={
+              userData?.metadata?.is_app_request_quote_notification_on == 1
+                ? true
+                : false
+            }
+            emailStatus={
+              userData?.metadata?.is_email_request_quote_notification_on == 1
+                ? true
+                : false
+            }
+            question={"When a customer requests a quote"}
+            appKey="is_app_request_quote_notification_on"
+            emailKey="is_email_request_quote_notification_on"
+          />
+          <NotificationSettingPro
+            appStatus={
+              userData?.metadata?.is_app_review_notification_on == 1
+                ? true
+                : false
+            }
+            emailStatus={
+              userData?.metadata?.is_email_review_notification_on == 1
+                ? true
+                : false
+            }
+            question={"When a review has been received "}
+            appKey="is_app_review_notification_on"
+            emailKey="is_email_review_notification_on"
+          />
+          {/* <NotificationSettingPro
         appStatus={
           userData?.metadata?.is_ == 1 ? true : false
         }
@@ -58,36 +66,38 @@ function NotificationSettingDetailSectionPro() {
         appKey="is_"
         emailKey="is_email_lead_notification_on"
       /> */}
-      <NotificationSettingPro
-        appStatus={
-          userData?.metadata?.is_app_close_request_notification_on == 1
-            ? true
-            : false
-        }
-        emailStatus={
-          userData?.metadata?.is_email_close_request_notification_on == 1
-            ? true
-            : false
-        }
-        question={"When a customer closes a request I have purchased "}
-        appKey="is_app_close_request_notification_on"
-        emailKey="is_email_close_request_notification_on"
-      />
-      <NotificationSettingPro
-        appStatus={
-          userData?.metadata?.is_app_promotion_mail_notification_on == 1
-            ? true
-            : false
-        }
-        emailStatus={
-          userData?.metadata?.is_email_promotion_mail_notification_on == 1
-            ? true
-            : false
-        }
-        question={"Occasional promotional emails"}
-        appKey="is_app_promotion_mail_notification_on"
-        emailKey="is_email_promotion_mail_notification_on"
-      />
+          <NotificationSettingPro
+            appStatus={
+              userData?.metadata?.is_app_close_request_notification_on == 1
+                ? true
+                : false
+            }
+            emailStatus={
+              userData?.metadata?.is_email_close_request_notification_on == 1
+                ? true
+                : false
+            }
+            question={"When a customer closes a request I have purchased "}
+            appKey="is_app_close_request_notification_on"
+            emailKey="is_email_close_request_notification_on"
+          />
+          <NotificationSettingPro
+            appStatus={
+              userData?.metadata?.is_app_promotion_mail_notification_on == 1
+                ? true
+                : false
+            }
+            emailStatus={
+              userData?.metadata?.is_email_promotion_mail_notification_on == 1
+                ? true
+                : false
+            }
+            question={"Occasional promotional emails"}
+            appKey="is_app_promotion_mail_notification_on"
+            emailKey="is_email_promotion_mail_notification_on"
+          />
+        </div>
+      )}
     </div>
   );
 }
