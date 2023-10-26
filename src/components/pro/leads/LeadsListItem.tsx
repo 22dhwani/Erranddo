a119f9cccaca97291c585e0e198b72aa1adb93eb
dhwani22 <dhwanisheth2202@gmail.com>
2023-10-26 11:28:28 +0530

    changes

--- src/components/pro/leads/LeadsListItem.tsx
+++ src/components/pro/leads/LeadsListItem.tsx
@@ -79,11 +79,6 @@ function LeadsListItem(props: {
           }
         >
           <Heading
-            text={"hello"}
-            variant="subTitle"
-            headingclassname="!font-bold capitalize !text-base mx-1 tracking-wide dark:text-white"
-          />
-          <Heading
             text={props.title}
             variant="subTitle"
             headingclassname="!font-bold capitalize !text-base mx-1 tracking-wide dark:text-white"
